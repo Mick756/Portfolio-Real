773fb6f8aedfb67a159d6d8ff4d0b2ca1383a32b
Mick Arias <mick756@gmail.com>
2019-10-10 15:19:40 -0700

    Added more tabs and information

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,6 +10,18 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "..", "public", "pages", "contact.html"))
     });
 
+    app.get("/resume", (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "public", "pages", "resume.html"))
+    });
+
+    app.get("/skills", (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "public", "pages", "skills.html"))
+    });
+
+    app.get("/social", (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "public", "pages", "social.html"))
+    });
+
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "..", "public", "pages", "about.html"))
     });
